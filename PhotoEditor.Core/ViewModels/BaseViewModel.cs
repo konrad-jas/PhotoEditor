c24ee1debcf0860416ed2cbfbac25f6f5eef226e
Konrad <konrjas@gmail.com>
2016-06-16 19:48:37 +0200

    Synchronize background calls

--- PhotoEditor.Core/ViewModels/BaseViewModel.cs
+++ PhotoEditor.Core/ViewModels/BaseViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using MvvmCross.Core.ViewModels;
 
@@ -6,6 +7,7 @@ namespace PhotoEditor.Core.ViewModels
 {
 	public abstract class BaseViewModel : MvxViewModel
 	{
+		private readonly AutoResetEvent _resetEvent = new AutoResetEvent(true);
 		private bool _working;
 
 		public bool Working
@@ -16,12 +18,14 @@ namespace PhotoEditor.Core.ViewModels
 
 		public async void RunInBackground<TArg>(Func<Task<TArg>> backgroundTask, Action<TArg> callbackAction = null)
 		{
+			await Task.Run(() => _resetEvent.WaitOne());
 			Working = true;
 			var result = await Task.Run(async () => await backgroundTask());
 			if (result != null)
 				callbackAction?.Invoke(result);
 
 			Working = false;
+			_resetEvent.Set();
 		}
 	}
 }
